f99b15734f1c31d919eac26a4a5c243e12663af5
cccheung <jasoncheung0915@gmail.com>
2016-04-23 03:34:49 +0800

    Added files via upload

--- src/java/database/RecycleToyDB.java
+++ src/java/database/RecycleToyDB.java
@@ -34,6 +34,7 @@ public class RecycleToyDB {
                 rt.setImage(rs.getString("image"));
                 rt.setPrice(rs.getFloat("price"));
                 rt.setDonatedBy(rs.getString("donatedBy"));
+                rt.setIsApproved(rs.getInt("isApproved"));
             }
             return rt;
         } catch (Exception ex) {
@@ -48,7 +49,7 @@ public class RecycleToyDB {
         boolean isSuccess = false;
         try {
             cnnct = ConnectionUtil.getConnection();
-            String preQueryStatement = "INSERT INTO \"RecycleToy\" (\"Name\", \"Description\", \"Category\", \"Image\", \"price\", \"DonatedBy\") VALUES(?, ?, ?, ?, ?, ?, ?)";
+            String preQueryStatement = "INSERT INTO \"RecycleToy\" (\"Name\", \"Description\", \"Category\", \"Image\", \"price\", \"DonatedBy\", \"IsApproved\") VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
             pStmnt = cnnct.prepareStatement(preQueryStatement);
             pStmnt.setString(1, rt.getName());
             pStmnt.setString(2, rt.getDescription());
@@ -56,6 +57,7 @@ public class RecycleToyDB {
             pStmnt.setString(4, rt.getImage());
             pStmnt.setFloat(5, rt.getPrice());
             pStmnt.setString(6, rt.getDonatedBy());
+            pStmnt.setInt(7, rt.getIsApproved());
             int rowCount = pStmnt.executeUpdate();
 
             if (rowCount >= 1) {
@@ -82,7 +84,7 @@ public class RecycleToyDB {
         boolean isSuccess = false;
         try {
             cnnct = ConnectionUtil.getConnection();
-            String preQueryStatement = "UPDATE \"RecycleToy\" SET \"Name\"=?, \"Description\"=?, \"Category\"=?, \"Image\"=?, \"price\"=?, \"DonatedBy\"=? WHERE \"tid\"=?";
+            String preQueryStatement = "UPDATE \"RecycleToy\" SET \"Name\"=?, \"Description\"=?, \"Category\"=?, \"Image\"=?, \"price\"=?, \"DonatedBy\"=?, \"IsApproved\" WHERE \"tid\"=?";
             pStmnt = cnnct.prepareStatement(preQueryStatement);
             pStmnt.setString(1, rt.getName());
             pStmnt.setString(2, rt.getDescription());
@@ -90,6 +92,7 @@ public class RecycleToyDB {
             pStmnt.setString(4, rt.getImage());
             pStmnt.setFloat(5, rt.getPrice());
             pStmnt.setString(6, rt.getDonatedBy());
+            pStmnt.setInt(7, rt.getIsApproved());
             int rowCount = pStmnt.executeUpdate();
 
             if (rowCount >= 1) {
@@ -172,40 +175,8 @@ public class RecycleToyDB {
                 rt.setImage(rs.getString("image"));
                 rt.setPrice(rs.getFloat("price"));
                 rt.setDonatedBy(rs.getString("donatedBy"));
-				
-                al.add(rt);
-            }
-            return al;
-        } catch (Exception ex) {
-            System.out.println(ex.toString());
-            return al;
-        }
-    }
-    
-    public ArrayList<RecycleToy> searchRToy(String searchString) {
-        Connection cnnct = null;
-        PreparedStatement pStmnt = null;
-        ArrayList<RecycleToy> al = new ArrayList<RecycleToy>();
-        searchString = "%" + searchString.toLowerCase() + "%";
-
-        try {
-            cnnct = ConnectionUtil.getConnection();
-            String preQueryStatement = "SELECT * FROM \"RecycleToy\" WHERE LOWER(\"Name\") LIKE ? OR LOWER(\"Description\") LIKE ? OR LOWER(\"Category\") LIKE ?";
-            pStmnt = cnnct.prepareStatement(preQueryStatement);
-            pStmnt.setString(1, searchString);
-            pStmnt.setString(2, searchString);
-            ResultSet rs = pStmnt.executeQuery();
-
-            while (rs.next()) {
-                RecycleToy rt = new RecycleToy();
-                rt.setTid(rs.getInt("tid"));
-                rt.setName(rs.getString("name"));
-                rt.setDescription(rs.getString("description"));
-                rt.setCategory(rs.getString("category"));
-                rt.setImage(rs.getString("image"));
-                rt.setPrice(rs.getFloat("price"));
-                rt.setDonatedBy(rs.getString("donatedBy"));
-				
+		rt.setIsApproved(rs.getInt("isApproved"));	
+                
                 al.add(rt);
             }
             return al;
@@ -236,6 +207,7 @@ public class RecycleToyDB {
                 rt.setImage(rs.getString("image"));
                 rt.setPrice(rs.getFloat("price"));
                 rt.setDonatedBy(rs.getString("donatedBy"));
+                rt.setIsApproved(rs.getInt("isApproved"));
 				
                 al.add(rt);
                 i++;
@@ -244,6 +216,94 @@ public class RecycleToyDB {
         } catch (Exception ex) {
             System.out.println(ex.toString());
             return al;
+        }
+    }
+     
+     public boolean approveRecycle(int tid, int isApproved) {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+        boolean isSuccess = false;
+        try {
+            cnnct = ConnectionUtil.getConnection();
+            String preQueryStatement = "UPDATE \"RecycleToy\" SET \"IsApproved\" = ? WHERE \"Tid\" = ? ";
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            RecycleToy rt = new RecycleToy();
+            pStmnt.setInt(1, rt.getIsApproved());
+            pStmnt.setInt(2, rt.getTid());
+            
+            int rowCount = pStmnt.executeUpdate();
+
+            if (rowCount >= 1) {
+                isSuccess = true;
+            }
+            pStmnt.close();
+            cnnct.close();
+        } catch (SQLException ex) {
+            while (ex != null) {
+                ex.printStackTrace();
+                ex = ex.getNextException();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ToyInventoryDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return isSuccess;
+    }
+    
+      public  ArrayList<RecycleToy> getRToyByCategory(String category) {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+        ArrayList<RecycleToy> al = new ArrayList<RecycleToy>();
+
+        try {
+            cnnct = ConnectionUtil.getConnection();
+            String preQueryStatement = "SELECT * FROM \"RecycleToy\" WHERE \"Category\" = ? ORDER BY \"tid\" DESC";
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            pStmnt.setString(1, category);
+            ResultSet rs = pStmnt.executeQuery();
+
+            while (rs.next()) {
+                RecycleToy t = new RecycleToy();
+                t.setTid(rs.getInt("tid"));
+                t.setName(rs.getString("Name"));
+                t.setDescription(rs.getString("Description"));
+                t.setCategory(rs.getString("Category"));
+                t.setImage(rs.getString("Image"));
+                t.setPrice(rs.getFloat("Price"));
+                t.setDonatedBy(rs.getString("donatedBy"));
+				
+                al.add(t);
+            }
+            return al;
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            return al;
+        }
+    }
+        
+        public  ArrayList<String> getAllCategory() {
+        Connection cnnct = null;
+        PreparedStatement pStmnt = null;
+        ArrayList<String> s=new ArrayList<String>();
+
+        try {
+            cnnct = ConnectionUtil.getConnection();
+            String preQueryStatement = "SELECT DISTINCT Category FROM \"RecycleToy\"";
+            pStmnt = cnnct.prepareStatement(preQueryStatement);
+            ResultSet rs = pStmnt.executeQuery();
+            
+            while (rs.next()) {
+                
+                s.add(rs.getString("Category"));
+                
+            }
+            
+            System.out.println("S: "+s);
+            return s;
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            return s;
         }
     }
 }
